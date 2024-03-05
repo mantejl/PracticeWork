8322b22bcf1e343d9406bf7d782d6f98d1c50d50
Mantej Lamba <mlamba@usc.edu>
2024-03-05 00:35:21 -0800

    start

--- TwoSum.py
+++ TwoSum.py
@@ -1,16 +1,24 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        numbers = {}
+from typing import List
 
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # brute force approach
+        # nested for loop that searches through entire list and checks all possible
+        # combinations to see which two numbers add up to target
+
+        # optimized approach would be to loop through list and have map of nums and
+        # their respective indexes
+        nums_index = {}
         for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in numbers:
-                return [numbers[complement], i]
-            numbers[nums[i]] = i
+            nums_index[nums[i]] = i
 
-        return []
+        print(nums_index.keys())
+
+        # loop through the list and subtract current number from target and check
+        # if complement exists in map
+
+        for i in range(len(nums) - 1):
+            comp = target - nums[i]
+            if comp in nums_index.keys() and i != nums_index[comp]:
+                return [i, nums_index[comp]]
